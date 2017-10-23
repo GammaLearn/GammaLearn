3390c1d3560bca1cbc49721d06254693f7488142
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2017-10-23 11:19:55 +0200

    just changed the name to something more general

--- hdf5_converter/prun2hdf5.py
+++ hdf5_converter/prun2hdf5.py
@@ -25,16 +25,41 @@ def load_calibrated_prun(filename):
     return pr
 
 
-
-
-
-
-def energyband_extractor(prunfilename):
+def extract_images_telescope(pcalibrun_filename, telescope_type):
     """
+    Extract images from a calibrated file for a unique telescope type
+    and stack them in a numpy array
+    Parameters
+    ----------
+    pcalibrun_filename: string
+    telescope_type: int
+
+    Returns
+    -------
+    2D Numpy array
+    """
+    pr = PCalibRun()
+    pr.load(pcalibrun_filename)
+
+    return np.array([event.tabPixel for tel in pr.tabTelescope for event in tel.tabTelEvent if tel.telescopeType == telescope_type])
+
+
+
+
+
+def energyband_extractor(pcalibrun_filename, psimu_filename, emin, emax):
+
+    """
+    Extract images from a pcalibrun file for events in a given energy band.
+    Simulated energy is given by the psimu file
+    Return a dictionnary with stacked images per telescope type
 
     Parameters
     ----------
-    prunfilename
+    pcalibrun_filename: string
+    psimu_filename: string
+    emin: float
+    emax: float
 
     Returns
     -------

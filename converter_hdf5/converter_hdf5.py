68d8d47d9722a0ddb3a5bf75a9e14a2ace5b8b5a
mikael <mikael.jacquemont@lapp.in2p3.fr>
2017-11-02 13:35:13 +0100

    Move conversion execution to conversion.py

--- converter_hdf5/converter_hdf5.py
+++ converter_hdf5/converter_hdf5.py
@@ -6,8 +6,6 @@ from hipecta import core
 import numpy as np
 import h5py
 
-import argparse
-
 import hashlib
 
 import sys
@@ -457,46 +455,3 @@ def browse_folder(data_folder):
         print("! ignoring psimu files (no corresponding pcalibrun) !")
 
     return file_set
-
-
-# Example of use. Need to define the way to get the file names
-# pruncalibfilename = '/home/jacquemont/projets_CTA/Prod3b/Paranal/Gamma_point_source/gamma_20deg_0deg_run4514___cta-prod3_desert-2150m-Paranal-merged.pcalibRun'
-# simufilename = '/home/jacquemont/projets_CTA/Prod3b/Paranal/Gamma_point_source/gamma_20deg_0deg_run4514___cta-prod3_desert-2150m-Paranal-merged.psimu'
-# hdf5filename = '/home/jacquemont/projets_CTA/gamma.hdf5'
-
-parser = argparse.ArgumentParser()
-# parser.add_argument("pcalibrunfile", help="pcalibrun file name to read")
-# parser.add_argument("psimulation", help="psimulation file name to read")
-parser.add_argument("data_folder", help="path to folder of pcalibrun and psimu files")
-parser.add_argument("hdf5file", help="hdf5 file name (including path) to write")
-args = parser.parse_args()
-#pruncalibfilename = args.pcalibrunfile
-#simufilename = args.psimulation
-data_folder = args.data_folder + '/'
-hdf5filename = args.hdf5file
-
-
-#cta_to_hdf5(pruncalibfilename, simufilename, hdf5filename)
-# dic=extract_random_image_data_from_hdf5(hdf5filename)
-# path_to_files = '/home/jacquemont/projets_CTA/Prod3b/Paranal/Gamma_point_source/'
-# extract_image_data_from_pcalibrun(dic,path_to_files)
-
-prset = browse_folder(data_folder)
-prlist = list(prset)
-prlist.sort()
-chunk_size = 10
-pr_chunks = [prlist[i:i + chunk_size] for i in range(0, len(prlist), chunk_size)]
-for i, chunk in enumerate(pr_chunks):
-    print("Process chunk %d over %d" % (i, len(pr_chunks)))
-    hdf5name, ext = os.path.splitext(hdf5filename)
-    hdf5name += str(i)
-    hdf5name += ext
-    for file in chunk:
-        print("Convert file : ", file)
-        cta_to_hdf5(file + ".pcalibrun", file + ".psimu", hdf5name)
-    # Random checking of data in hdf5 file
-    print("Ckeck conversion")
-    print("Extract random image data from hdf5 file : ", hdf5name)
-    dic = extract_random_image_data_from_hdf5(hdf5name)
-    print("Extract related data in pcalibrun and psimu files")
-    extract_image_data_from_pcalibrun(dic, data_folder)
